78e73c9aa5f365cd4acf21f3782a51bf1252d683
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-22 17:48:18 +0300

    create components

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,35 +1,36 @@
 /*eslint-disable*/
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './PhotoCard.module.css';
 
 const PhotoCard = () => (
-  <div className="photo-card">
-  <img src="" alt="" />
+  <div className={styles.photoCard}>
+    <img src="" alt="" />
 
-  <div className="stats">
-    <p className="stats-item">
-      <i className="material-icons">thumb_up</i>
-      1108
-    </p>
-    <p className="stats-item">
-      <i className="material-icons">visibility</i>
-      320321
-    </p>
-    <p className="stats-item">
-      <i className="material-icons">comment</i>
-      129
-    </p>
-    <p className="stats-item">
-      <i className="material-icons">cloud_download</i>
-      176019
-    </p>
+    <div className={styles.stats}>
+      <p className={styles.statsItem}>
+        <i className={styles.materialIcons}>thumb_up</i>
+        1108
+      </p>
+      <p className={styles.statsItem}>
+        <i className={styles.materialIcons}>visibility</i>
+        320321
+      </p>
+      <p className={styles.statsItem}>
+        <i className={styles.materialIcons}>comment</i>
+        129
+      </p>
+      <p className={styles.statsItem}>
+        <i className={styles.materialIcons}>cloud_download</i>
+        176019
+      </p>
+    </div>
+
+    {/* <!-- Кнопка для открытия модалки с большим изображением, появляется при наведении --> */}
+    <button type="button" className={styles.fullscreenButton}>
+      <i className={styles.materialIcons}>zoom_out_map</i>
+    </button>
   </div>
-
-  <!-- Кнопка для открытия модалки с большим изображением, появляется при наведении -->
-  <button type="button" class="fullscreen-button">
-    <i className="material-icons">zoom_out_map</i>
-  </button>
-</div>
 );
 
 export default PhotoCard;
